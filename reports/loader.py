41fef22323da4bdc16db7f554c1b22734f37d81f
Matt Waite <matt.waite@gmail.com>
2017-06-08 22:39:00 -0500

    Tweaked loader to give you some hints

--- reports/loader.py
+++ reports/loader.py
@@ -17,9 +17,10 @@ for row in reader:
     refstate_slug = slugify(refstate)
     stateid, stateadded = State.objects.get_or_create(name=refstate, name_slug=refstate_slug)
 
-    refyear = row[1]
-    refyear_slug = slugify(refyear)
-    refyearid, refyearadded = Year.objects.get_or_create(name=refyear, name_slug=refyear_slug)
+    # Year is just a field, not a model, do you don't need this.
+    #refyear = row[1]
+    #refyear_slug = slugify(refyear)
+    #refyearid, refyearadded = Year.objects.get_or_create(name=refyear, name_slug=refyear_slug)
 
     reforigin = row[3]
     reforigin_slug = slugify(reforigin)
@@ -28,7 +29,7 @@ for row in reader:
     popcity =row[6]
     popcity_slug = slugify (popcity)
     popcityid, popcityadded = City.objects.get_or_create(name=popcity, name_slug=popcity_slug)
-
-    ref = RefugeeReport(State=refstate, Year=refyear, Country=reforigin, City=popcity)
-    print(ref)
+    # for sanity, you want to keep the RefugeeReport fields in the same order as your model)
+    ref = RefugeeReport(country=reforigin, state=refstate, city=popcity, year=row[1], city_total=row[?], state_total=row[?], country_total=row[?], all_countries=[?])
+    #print(ref)
     ref.save()
