910296f97ebf1962a13b5cdeb3af41d59263ad08
raine17 <ecase@huskers.unl.edu>
2017-06-12 11:46:20 -0400

    fixing loader script

--- reports/models.py
+++ reports/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
 class Country(models.Model):
-    name=models.CharField(max_length=255)
+    name=models.CharField(max_length=255, blank=True, null=True)
     name_slug=models.SlugField()
     def __str__(self):
         return self.name
 
 class State(models.Model):
-    name=models.CharField(max_length=255)
+    name=models.CharField(max_length=255, blank=True, null=True)
     name_slug=models.SlugField()
     def __str__(self):
         return self.name
@@ -15,8 +15,8 @@ class State(models.Model):
         return "/reports/%s" % self.name_slug
 
 class City(models.Model):
-    state=models.ForeignKey(State)
-    name=models.CharField(max_length=255)
+    state=models.ForeignKey(State, on_delete=models.CASCADE)
+    name=models.CharField(max_length=255, blank=True, null=True)
     name_slug=models.SlugField()
     def __str__(self):
         return self.name
@@ -25,13 +25,13 @@ class City(models.Model):
 
 class RefugeeReport(models.Model):
     country=models.ForeignKey(Country)
-    state=models.ForeignKey(State)
-    city=models.ForeignKey(City)
-    year=models.CharField(max_length=255)
-    city_total=models.IntegerField(default=0)
-    state_total=models.IntegerField(default=0)
-    country_total=models.IntegerField(default=0)
-    all_countries=models.IntegerField(default=0)
+    state=models.ForeignKey(State, blank=True, null=True)
+    city=models.ForeignKey(City, blank=True, null=True)
+    year=models.CharField(max_length=255, blank=True, null=True)
+    city_total=models.IntegerField(default=0, null=True)
+    state_total=models.IntegerField(default=0, null=True)
+    country_total=models.IntegerField(default=0, null=True)
+    all_countries=models.IntegerField(default=0, null=True)
     def __str__(self):
         return "%s, %s from %s" % (self.city.name_slug, self.state.name_slug, self.year)
     def get_absolute_url(self):
